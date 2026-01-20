1bc8f8d10f461039f56ad3636a2f92bc431b3d29
Raxxxtv <raxtv.mail@gmail.com>
2026-01-20 23:19:48 +0100

    Optimizing

--- client.lua
+++ client.lua
@@ -2,29 +2,29 @@ ESX = exports["es_extended"]:getSharedObject()
 
 local marker = {}
 
-local function CreateMarker()
+local function CreateMarker() -- Creates Markers with the mg_lib (markerlib) once and uses the settings of the Config
     for id, data in pairs(Config.Marker) do
         if marker[id] then
             goto continue
         end
         marker[id] = exports.mg_lib:AddMarker({
 	    	name = id,
-	    	type = data.type,
+	    	type = data.type or Config.DefaultMarker.type,
 	    	pos = data.pos,
-	    	size = data.size,
-	    	color = data.color,
-	    	helpNotification = data.helpMessage,
+	    	size = data.size or Config.DefaultMarker.size,
+	    	color = data.color or Config.DefaultMarker.color,
+	    	helpNotification = data.helpMessage or Config.DefaultMarker.helpMessage,
 	    	onUse = function()
-	    		SetEntityCoords(ESX.PlayerData.ped, Config.Coords)
+	    		SetEntityCoords(ESX.PlayerData.ped, Config.TeleportCoords)
 	    	end,
-            offset = data.offset,
-            useKey = data.useKey,
-            useDelay = data.useDelay,
-            drawDistance = data.drawDistance,
-            distance = data.distance,
-            faceCamera = data.faceCamera,
-            bobUpAndDown = data.bobUpAndDown,
-            rotate = data.rotate
+            offset = data.offset or Config.DefaultMarker.offset,
+            useKey = data.useKey or Config.DefaultMarker.useKey,
+            useDelay = data.useDelay or Config.DefaultMarker.useDelay,
+            drawDistance = data.drawDistance or Config.DefaultMarker.drawDistance,
+            distance = data.distance or Config.DefaultMarker.distance,
+            faceCamera = data.faceCamera or Config.DefaultMarker.faceCamera,
+            bobUpAndDown = data.bobUpAndDown or Config.DefaultMarker.bobUpAndDown,
+            rotate = data.rotate or Config.DefaultMarker.rotate
 	    })
         ::continue::
     end
--- config.lua
+++ config.lua
@@ -1,126 +1,64 @@
+-- Most Comments are help Comments from the mg_lib (markerlib)
+
 Config = {}
+
+Config.DefaultMarker = { -- Default Marker settings, can be overwritten in Config.Marker
+    type = 1, -- Marker type (Default/fallback is 1)
+    size = {1.2, 1.2, 1.0}, -- Size of marker.
+    color = {255, 0, 0, 180}, -- A table with 4 rgba elements. Alpha is optional and will fallback to 200.
+    helpMessage = "Drücke ~INPUT_CONTEXT~ um dich zu Teleportieren", -- Message that plops up when you get into the distance range.
+    offset = 0, -- Optional offset of marker, the default is -1 z axis (Ground position of player peds). (Can also be a number which only represents Z axis)
+    useKey = 51, -- Optional key override. (by default "E") | 0 or false to disable this behaviour. (This calls DisableControlAction, if within reach)
+    useDelay = 1000, -- In ms. Use delay after pressing use.
+    drawDistance = 20, -- Draw distance which falls back to 20.
+    distance = 5.0, -- Distance, where you can press the Key to use the marker
+    faceCamera = false, -- Marker should face camera?
+    bobUpAndDown = false, -- Should the marker bob up and down?
+    rotate = false -- Should the marker rotate?
+}
 
 Config.Marker = {
     ['marker1'] = {
-        pos = vector3(138.7424, -890.6888, 30.4260),
+        pos = vector3(138.7424, -890.6888, 30.4260), -- Vector3 position where the marker is placed.
         type = 1,
-        size = {1.2, 1.2, 1.0},
-        color = {255, 0, 0, 180},
-        helpMessage = "Drücke ~INPUT_CONTEXT~ um dich zu Teleportieren",
-        offset = 0,
-        useKey = 51,
-        useDelay = 1000,
-        drawDistance = 20,
-        distance = 5.0,
-        faceCamera = false,
-        bobUpAndDown = false,
-        rotate = false
+        color = {255, 0, 0, 180}
     },
     ['marker2'] = {
         pos = vector3(139.7424, -890.6888, 30.4260),
         type = 2,
-        size = {1.2, 1.2, 1.0},
-        color = {0, 255, 0, 180},
-        helpMessage = "Drücke ~INPUT_CONTEXT~ um dich zu Teleportieren",
-        offset = 0,
-        useKey = 51,
-        useDelay = 1000,
-        drawDistance = 20,
-        distance = 5.0,
-        faceCamera = false,
-        bobUpAndDown = false,
-        rotate = false
+        size = {1.0, 1.8, 1.2},
+        color = {0, 255, 0, 180}
     },
     ['marker3'] = {
         pos = vector3(140.8807, -890.6609, 30.4349),
         type = 3,
-        size = {1.2, 1.2, 1.0},
-        color = {0, 0, 255, 180},
-        helpMessage = "Drücke ~INPUT_CONTEXT~ um dich zu Teleportieren",
-        offset = 0,
-        useKey = 51,
-        useDelay = 1000,
-        drawDistance = 20,
-        distance = 5.0,
-        faceCamera = false,
-        bobUpAndDown = false,
-        rotate = false
+        color = {0, 0, 255, 180}
     },
     ['marker4'] = {
         pos = vector3(141.8807, -890.6609, 30.4349),
         type = 4,
-        size = {1.2, 1.2, 1.0},
-        color = {255, 255, 0, 180},
-        helpMessage = "Drücke ~INPUT_CONTEXT~ um dich zu Teleportieren",
-        offset = 0,
-        useKey = 51,
-        useDelay = 1000,
-        drawDistance = 20,
-        distance = 5.0,
-        faceCamera = false,
-        bobUpAndDown = false,
-        rotate = false
+        color = {255, 255, 0, 180}
     },
     ['marker5'] = {
         pos = vector3(142.8807, -890.6609, 30.4349),
         type = 5,
-        size = {1.2, 1.2, 1.0},
-        color = {255, 0, 255, 180},
-        helpMessage = "Drücke ~INPUT_CONTEXT~ um dich zu Teleportieren",
-        offset = 0,
-        useKey = 51,
-        useDelay = 1000,
-        drawDistance = 20,
-        distance = 5.0,
-        faceCamera = false,
-        bobUpAndDown = false,
-        rotate = false
+        color = {255, 0, 255, 180}
     },
     ['marker6'] = {
         pos = vector3(143.8807, -890.6609, 30.4349),
         type = 6,
-        size = {1.2, 1.2, 1.0},
-        color = {255, 255, 255, 180},
-        helpMessage = "Drücke ~INPUT_CONTEXT~ um dich zu Teleportieren",
-        offset = 0,
-        useKey = 51,
-        useDelay = 1000,
-        drawDistance = 20,
-        distance = 5.0,
-        faceCamera = false,
-        bobUpAndDown = false,
-        rotate = false
+        color = {255, 255, 255, 180}
     },
     ['marker7'] = {
         pos = vector3(144.8807, -890.6609, 30.4349),
         type = 7,
-        size = {1.2, 1.2, 1.0},
-        color = {0, 255, 255, 180},
-        helpMessage = "Drücke ~INPUT_CONTEXT~ um dich zu Teleportieren",
-        offset = 0,
-        useKey = 51,
-        useDelay = 1000,
-        drawDistance = 20,
-        distance = 5.0,
-        faceCamera = false,
-        bobUpAndDown = false,
-        rotate = false
+        color = {0, 255, 255, 180}
     },
     ['marker8'] = {
-        pos = vector3(144.8807, -890.6609, 30.4349),
+        pos = vector3(145.8807, -890.6609, 30.4349),
         type = 8,
-        size = {1.2, 1.2, 1.0},
-        color = {0, 0, 0, 180},
-        helpMessage = "Drücke ~INPUT_CONTEXT~ um dich zu Teleportieren",
-        offset = 0,
-        useKey = 51,
-        useDelay = 1000,
-        drawDistance = 20,
-        distance = 5.0,
-        faceCamera = false,
-        bobUpAndDown = false,
-        rotate = false
+        color = {0, 0, 0, 180}
     },
 }
 
-Config.Coords = vector3(137.1535, -908.1205, 30.3019)
+Config.TeleportCoords = vector3(137.1535, -908.1205, 30.3019) -- When you use the Marker, it will Teleport you to this Coords
